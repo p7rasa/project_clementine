44a02990ba7857cfe5c81c43d339348deedeb3d5
p7rasa <derinkoruk@posta.mu.edu.tr>
2026-05-16 16:08:42 +0300

    atnazbekliyom
    hebele hübele

--- clementine/Assets/Scripts/PlayerMovement.cs
+++ clementine/Assets/Scripts/PlayerMovement.cs
@@ -12,6 +12,7 @@ public class PlayerMovement : MonoBehaviour
     public float gravity = 10f;
     public float lookSpeed = 2f;
     public float lookXLimit = 45f;
+
     public float defaultHeight = 2f;
     public float crouchHeight = 1f;
     public float crouchSpeed = 3f;
@@ -25,6 +26,7 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         characterController = GetComponent<CharacterController>();
+
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
@@ -35,9 +37,15 @@ public class PlayerMovement : MonoBehaviour
         Vector3 right = transform.TransformDirection(Vector3.right);
 
         bool isRunning = Input.GetKey(KeyCode.LeftShift);
-        float curSpeedX = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Vertical") : 0;
-        float curSpeedY = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Horizontal") : 0;
+
+        float curSpeedX = canMove ?
+            (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Vertical") : 0;
+
+        float curSpeedY = canMove ?
+            (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Horizontal") : 0;
+
         float movementDirectionY = moveDirection.y;
+
         moveDirection = (forward * curSpeedX) + (right * curSpeedY);
 
         if (Input.GetButton("Jump") && canMove && characterController.isGrounded)
@@ -54,28 +62,40 @@ public class PlayerMovement : MonoBehaviour
             moveDirection.y -= gravity * Time.deltaTime;
         }
 
-        if (Input.GetKey(KeyCode.R) && canMove)
-        {
-            characterController.height = crouchHeight;
-            walkSpeed = crouchSpeed;
-            runSpeed = crouchSpeed;
+       
 
-        }
-        else
-        {
-            characterController.height = defaultHeight;
             walkSpeed = 6f;
             runSpeed = 12f;
-        }
+    
 
         characterController.Move(moveDirection * Time.deltaTime);
 
         if (canMove)
         {
             rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
+
             rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
-            playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
-            transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
+
+            playerCamera.transform.localRotation =
+                Quaternion.Euler(rotationX, 0, 0);
+
+            transform.rotation *= Quaternion.Euler(
+                0,
+                Input.GetAxis("Mouse X") * lookSpeed,
+                0
+            );
         }
+
+        if (Input.GetKeyDown(KeyCode.E))
+{
+    Debug.Log("E calisti");
+
+     Alarm alarm = FindFirstObjectByType<Alarm>();
+
+    if (alarm != null)
+    {
+        alarm.ActivateAlarm();
+    }
+}
     }
 }
